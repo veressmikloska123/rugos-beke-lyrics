7df9e1350e6168a7b269d49c7957c3efaa0f60f1
veressmikloska123 <54560128+veressmikloska123@users.noreply.github.com>
2022-10-31 19:38:24 +0100

    Add error handling for readscript

--- reader.py
+++ reader.py
@@ -2,9 +2,17 @@
 
 import sys
 
-if(sys.argv[1] == "y"):
- print("There is need for help")
- with open(sys.argv[2], 'r') as f:
-  print(f.read())
+if len(sys.argv) > 1:
+ if(sys.argv[1] == "y"):
+  print("There is need for help")
+  if len(sys.argv) > 2:
+   with open(sys.argv[2], 'r') as f:
+    print(f.read())
+  else:
+   print("WARNING: You do not know how it works. Refrain from doing that again.")
+ else:
+  print("WARNING: You do not know if this is for you. Please refrain from using this code block anytime after.")
 else:
  print("WARNING: This code reads rugos beke lyrics. To continue, use the y modifier to do so.")
+
+ print("Lesser instruments: for a better life. no limits in underlife")
