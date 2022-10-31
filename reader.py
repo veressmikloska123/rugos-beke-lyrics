cdc992acc91b8c7332a0d1ee9e6e3d9d9d2f55a1
veressmikloska123 <54560128+veressmikloska123@users.noreply.github.com>
2022-10-31 19:30:53 +0100

    FUCK
    I FUCKING HATE PYTHON

--- reader.py
+++ reader.py
@@ -4,8 +4,7 @@ import sys
 
 print("WARNING: This code reads rugos beke lyrics. To continue, use the y modifier to do so.")
 
-if(sys.argv[1] == "y"){
+if(sys.argv[1] == "y")
  print("There is need for help")
-  with open(sys.argv[2], 'r') as f:
-    print(f.read())
-}
+ with open(sys.argv[2], 'r') as f:
+  print(f.read())
